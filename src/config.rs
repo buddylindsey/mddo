bba7b289fad3c260fab68decafc1fd0bc673eef2
Buddy Lindsey, Jr <buddylindsey@users.noreply.github.com>
2024-05-29 22:05:50 -0500

    Refactor config creation

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,7 @@
 use dirs::{config_dir, document_dir};
 use serde::{Deserialize, Serialize};
 use std::fs;
-use std::io::Error;
+use std::io::{Error, ErrorKind};
 use toml;
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -9,26 +9,12 @@ pub struct Config {
     pub data_location: String,
 }
 
-fn create_default_config() -> Result<(), Error> {
-    let mut config_path = config_dir()
-        .ok_or("Could not find config directory")
-        .unwrap();
-
-    config_path.push("mddo");
-    fs::create_dir_all(&config_path)?;
-
-    config_path.push("config.toml");
+fn create_default_config(config_path: &std::path::Path) -> Result<(), Error> {
+    let data_dir = document_dir().ok_or_else(|| Error::new(ErrorKind::NotFound, "Could not find document directory"))?;
+    let data_location = format!("{}/mddo/", data_dir.to_str().unwrap());
 
     let default_config = Config {
-        data_location: format!(
-            "{}/mddo/",
-            document_dir()
-                .ok_or("Could not find document directory")
-                .unwrap()
-                .to_str()
-                .unwrap()
-                .to_string()
-        ),
+        data_location
     };
 
     let _ = fs::write(config_path, toml::to_string(&default_config).unwrap());
@@ -40,12 +26,16 @@ pub fn load_config() -> Result<Config, Error> {
     let mut config_path = config_dir()
         .ok_or("Could not find config directory")
         .unwrap();
-
     config_path.push("mddo");
+
+    if !config_path.try_exists().is_err() {
+        fs::create_dir(&config_path)?;
+    }
+
     config_path.push("config.toml");
 
     if !config_path.try_exists().is_err() {
-        let _ = create_default_config();
+        create_default_config(&config_path)?;
     }
 
     let config = fs::read_to_string(config_path)?;
